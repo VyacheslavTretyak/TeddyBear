0f6cac696f9bfaa073b63fefdf4f921793aec377
VyacheslavT <tretyak1c@gmail.com>
2018-05-16 18:34:52 +0300

    Init project

---
+++ Scripts/SlidePanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidePanel : MonoBehaviour {
+	public Animator animator;
+	private RectTransform rectTr;
+	void Start () {
+		animator = GetComponent<Animator>();
+		rectTr = GetComponent<RectTransform>();
+	}	
+	public void SetHeightPosition(float height)
+	{
+		rectTr.anchoredPosition = new Vector2(rectTr.anchoredPosition.x, height);
+	}
+}
---
+++ Scripts/btnColorPick.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class btnColorPick : MonoBehaviour {
+	public ImagePrefab imgPref;
+	public Sprite sprite;
+	public Color[] colors;
+
+	private SlidePanel slidePanel;
+	private RectTransform rectTr;
+	private float height;
+	void Start () {
+		slidePanel = FindObjectOfType<SlidePanel>();
+		rectTr = GetComponent<RectTransform>();
+		height = rectTr.anchoredPosition.y;
+	}		
+	public void OnClick()
+	{
+		slidePanel.SetHeightPosition(height);
+		slidePanel.animator.SetBool("isShow", !slidePanel.animator.GetBool("isShow"));
+		LoadColors();
+
+	}
+	private void LoadColors()
+	{
+		foreach (Color col in colors)
+		{
+			ImagePrefab imageObj = Instantiate(imgPref);
+			Image img = imageObj.GetComponent<Image>();
+			img.sprite = sprite;			
+			img.color = col;
+			imageObj.GetComponent<RectTransform>().anchoredPosition = rectTr.anchoredPosition;
+			imageObj.transform.SetParent(slidePanel.transform);
+		}
+	}
+}
